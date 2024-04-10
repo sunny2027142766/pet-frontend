2e03b582bc9e24e6f7836022ea7b374d1d833a75
sunny2027142766 <2027142766@qq.com>
2024-04-10 18:03:29 +0800

    blander模型导入测试

--- src/views/front/interaction/index.tsx
+++ src/views/front/interaction/index.tsx
@@ -1,13 +1,74 @@
-import { Box } from '@mui/material'
+import { Suspense, useRef } from 'react'
+import { OrbitControls } from '@react-three/drei'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+// @ts-expect-error TS_E
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { Mesh } from 'three'
+
+const Model = () => {
+  const fileUrl = '/shiba/scene.gltf'
+  const mesh = useRef<Mesh>(null!)
+  const gltf = useLoader(GLTFLoader, fileUrl)
+
+  useFrame(() => {
+    mesh.current.rotation.y += 0.01
+  })
+
+  return (
+    <mesh ref={mesh}>
+      <primitive object={gltf.scene} />
+    </mesh>
+  )
+}
+const DogModel = () => {
+  const fileUrl = '/dog/scene.gltf'
+  const mesh = useRef<Mesh>(null!)
+  const gltf = useLoader(GLTFLoader, fileUrl)
+  console.log(gltf)
+
+  return (
+    <mesh ref={mesh}>
+      <primitive object={gltf.scene} />
+    </mesh>
+  )
+}
+
+const KejiModel = () => {
+  const fileUrl = '/keji/scene.gltf'
+  const mesh = useRef<Mesh>(null!)
+  const gltf = useLoader(GLTFLoader, fileUrl)
+  console.log(gltf)
+
+  return (
+    <mesh ref={mesh}>
+      <primitive object={gltf.scene} />
+    </mesh>
+  )
+}
+
+const CanvasDom = () => {
+  return (
+    <Canvas
+      style={{
+        width: '100%',
+        height: 'calc(100vh - 64px - 65px)',
+        backgroundColor: '#000'
+      }}
+    >
+      <Suspense fallback={null}>
+        <OrbitControls />
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        {/* <Model /> */}
+        <DogModel />
+        <KejiModel />
+      </Suspense>
+    </Canvas>
+  )
+}
 
 const Interaction = () => {
-  return (
-    <Box component="div" display="flex" flex="1" flexDirection="column">
-      <Box component="h1" sx={{ fontSize: '1rem', fontWeight: 'bold', margin: '1rem auto' }}>
-        宠物互动
-      </Box>
-    </Box>
-  )
+  return <CanvasDom />
 }
 
 export default Interaction
