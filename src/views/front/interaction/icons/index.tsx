d75eb87555318cceaab2f248bc3864c56243b34b
sunny2027142766 <2027142766@qq.com>
2024-04-15 18:01:10 +0800

    添加互动按钮

---
+++ src/views/front/interaction/icons/index.tsx
@@ -0,0 +1,3 @@
+export { default as PandasIcon } from './Pandas'
+export { default as TouchIcon } from './Touch'
+export { default as InterActionIcon } from './Interaction'
--- src/views/front/interaction/index.tsx
+++ src/views/front/interaction/index.tsx
@@ -1,18 +1,21 @@
-import { Suspense, useRef } from 'react'
+import { Suspense, useRef, useState } from 'react'
 import { OrbitControls } from '@react-three/drei'
 import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 // @ts-expect-error TS_E
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { Mesh } from 'three'
+import { CommentOutlined, CustomerServiceOutlined } from '@ant-design/icons'
+import { FloatButton } from 'antd'
+import { PandasIcon, TouchIcon, InterActionIcon } from './icons'
 
 const Model = () => {
   const fileUrl = '/shiba/scene.gltf'
   const mesh = useRef<Mesh>(null!)
   const gltf = useLoader(GLTFLoader, fileUrl)
 
-  useFrame(() => {
-    mesh.current.rotation.y += 0.01
-  })
+  // useFrame(() => {
+  //   mesh.current.rotation.y += 0.01
+  // })
 
   return (
     <mesh ref={mesh}>
@@ -24,7 +27,7 @@ const DogModel = () => {
   const fileUrl = '/dog/scene.gltf'
   const mesh = useRef<Mesh>(null!)
   const gltf = useLoader(GLTFLoader, fileUrl)
-  console.log(gltf)
+  // console.log(gltf)
 
   return (
     <mesh ref={mesh}>
@@ -37,8 +40,7 @@ const KejiModel = () => {
   const fileUrl = '/keji/scene.gltf'
   const mesh = useRef<Mesh>(null!)
   const gltf = useLoader(GLTFLoader, fileUrl)
-  console.log(gltf)
-
+  // console.log(gltf)
   return (
     <mesh ref={mesh}>
       <primitive object={gltf.scene} />
@@ -52,23 +54,46 @@ const CanvasDom = () => {
       style={{
         width: '100%',
         height: 'calc(100vh - 64px - 65px)',
-        backgroundColor: '#000'
+        background: '#c6e5db'
       }}
     >
       <Suspense fallback={null}>
         <OrbitControls />
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        {/* <Model /> */}
-        <DogModel />
-        <KejiModel />
+        <Model />
+        {/* <DogModel /> */}
+        {/* <KejiModel /> */}
       </Suspense>
     </Canvas>
   )
 }
 
+const HandleButtons = () => {
+  return (
+    <>
+      <FloatButton.Group
+        trigger="click"
+        type="default"
+        shape="square"
+        style={{ right: 24, bottom: 100 }}
+        icon={<InterActionIcon style={{ fontSize: '20px' }} />}
+        tooltip={<div>宠物互动</div>}
+      >
+        <FloatButton tooltip={<div>宠物展示</div>} icon={<PandasIcon style={{ fontSize: '20px' }} />} />
+        <FloatButton tooltip={<div>抚摸</div>} icon={<TouchIcon style={{ fontSize: '20px' }} />} />
+      </FloatButton.Group>
+    </>
+  )
+}
+
 const Interaction = () => {
-  return <CanvasDom />
+  return (
+    <>
+      <CanvasDom />
+      <HandleButtons />
+    </>
+  )
 }
 
 export default Interaction
